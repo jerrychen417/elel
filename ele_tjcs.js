20637f636532ca1b19cf588887ca0e2572001a3c
jerrychen417 <94837115+jerrychen417@users.noreply.github.com>
2024-08-19 10:22:33 +0800

    Update ele_tjcs.js

--- ele_tjcs.js
+++ ele_tjcs.js
@@ -1,7 +1,7 @@
 /**
  * @平行绳 飞机频道：https://t.me/tigerorrose
  * 变量：elmck: 必填，账号cookie，短信登录面板项目地址：https://github.com/funaihui/eleWeb
- * cron 0 0 11 * * *
+ * cron 1
  * 2023.6.26 更新：首次发布；
  * 2023.7.7 更新：添加延时，增加 cookie 失效验证
  */
