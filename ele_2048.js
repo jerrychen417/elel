edebb46a2f1b9c46349e9313d1cacf40a97a5166
jerrychen417 <94837115+jerrychen417@users.noreply.github.com>
2024-08-19 10:16:21 +0800

    Update ele_2048.js

--- ele_2048.js
+++ ele_2048.js
@@ -2,7 +2,7 @@
  * @平行绳 飞机频道：https://t.me/tigerorrose
  * 变量：elmck: 必填，账号cookie，短信登录面板项目地址：https://github.com/funaihui/eleWeb
  * 卡密地址：http://card.94wan.fun:90
- * cron: 11 8 * * *
+ * cron: 30 8,16 * * *
  * 2023.7.15 首次发布，需要安装 moment和 md5 依赖
  */
 
